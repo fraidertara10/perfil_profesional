cc9b04ae10f41980aab85b52195341d437621739
fraidertara10 <fraidertarazona10@gmail.com>
2023-11-25 17:59:15 -0500

    modification

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@ const pi = 3.1416;
 const pul = 2.54;
 // var para variables globales
 var days= 10;
-var name = "Wilder Duarte";
+var name = "Fraider Tarazona";
 // let para variables de bloque o locales 
 let count = 1;
 
@@ -15,9 +15,9 @@ let count = 1;
 // console.log
 console.log(name);
 // body - pantalla
-document.write(name + "######Wilder duarte");
-document.getElementById("box_one").innerHTML = "<h1>Wilder Duarte</h1>"
-document.getElementById("box_two").innerText = "<h1>Wilder Duarte</h1>"
+document.write(name + "######Fraider Tarazona");
+document.getElementById("box_one").innerHTML = "<h1>Fraider Tarazona</h1>"
+document.getElementById("box_two").innerText = "<h1>Fraider Tarazona</h1>"
 // librerias
 // Swal.fire({
 //      icon: 'error',
@@ -49,10 +49,10 @@ var array_num = [1,2,3,4,5,6];
 var array_text = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
 var array_mix = [1,"a",2,"c"];
 var array_mul = [
-    {name: "Wilder",last_name:"Duarte",age:"31"},
-    {name: "Anyi",last_name:"Martinez",age:"32"},
-    {name: "Celeste",last_name:"Duarte",age:"3"},
-    {name: "Antonella",last_name:"Duarte",age:"1"}
+    {name: "Fraider",last_name:"Tarazona",age:"20"},
+    {name: "Maria",last_name:"Jose",age:"23"},
+    {name: "Valery",last_name:"sofia",age:"5"},
+    {name: "angie",last_name:"Tarazona",age:"15"}
 ]
 
 // ############# operadores basicos 
